b8ee099dae67365d58694c6583bae0842ba9c8c0
Bella <woo.bella@gmail.com>
2015-06-30 18:27:28 -0400

    working login/logout

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,24 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all  
+  end
+
   def show
     @list = List.find params[:id]
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = current_user.list.new(
+      name: params[:name]
+      )
+    if @list.save
+      redirect_to list_path(@list), notice: "List created"
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root to: 'lists#index'
+
   resources :lists do
     resources :tasks do
       # patch '/lists/:list_id/tasks/:task_id/complete' => 'tasks#complete'
       patch :complete
     end
   end
-
-  root to: 'lists#index'
 end
